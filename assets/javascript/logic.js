bc4c24b6ac79e7330234e02d9486fa290d0a73fd
Kaviz101586 <jason.mapou@gmail.com>
2018-12-15 11:45:56 -0500

    phase 3

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -7,31 +7,51 @@ var config = {
     messagingSenderId: "853162754919"
   };
   firebase.initializeApp(config);
-
-var database = firebase.database();
-
-$(document).on("click", "#addUser", function(event) {
-    event.preventDefault();
-    console.log('hi');
-    // Capture User Inputs and store them into variables
-  var name = $('#newName').val().trim();
-  var role = $('#role').val().trim();
-  var date = $('#startDate').val().trim();
-  var rate = $('#rate').val().trim();
-
-    
-  database.ref("users").push({
-    name: name,
-    role: role,
-    date: date,
-    rate: rate,
-})
-})
-
-
-// var sv = snapshot.val()
-
-// $('#nameDisplay').text(sv.name)
-// $('#roleDisplay').text(sv.role)
-// $('#startDateDisplay').text(sv.date)
-// $('#rateDisplay').text(sv.rate)
+  
+  var database = firebase.database();
+  
+  $(document).on("click", "#addUser", function(event) {
+      event.preventDefault();
+      console.log('hi');
+      // Capture User Inputs and store them into variables
+    var name = $('#newName').val().trim();
+    var role = $('#role').val().trim();
+    var date = $('#startDate').val().trim();
+    var rate = $('#rate').val().trim();
+  
+      
+    database.ref("users").push({
+      name: name,
+      role: role,
+      date: date,
+      rate: rate,
+  })
+  })
+  database.ref("users").on("child_added", function(snapshot) {
+  
+    var userX = snapshot.val().name;
+    var roleX = snapshot.val().role;
+    var dateX = snapshot.val().date;
+    var rateX = snapshot.val().rate;
+  
+    var newRow = $("<tr>");
+    var newUser = $("<td>" + userX + "</td>");
+    var newRole = $("<td>" + roleX + "</td>");
+    var newDate = $("<td>" + dateX + "</td>");
+    var monthsWorked = $("<td>" + "" + "</td>");
+    var monthlyRate = $("<td>" + rateX + "</td>");
+    var totalBilled = $("<td>" + "" + "</td>");
+  
+  newRow.append(newUser).append(newRole).append(newDate).append(monthsWorked).append(monthlyRate).append(totalBilled);
+  
+  $("#tableBody").append(newRow);
+  
+  })
+  
+  // var sv = snapshot.val()
+  
+  // $('#nameDisplay').text(sv.name)
+  // $('#roleDisplay').text(sv.role)
+  // $('#startDateDisplay').text(sv.date)
+  // $('#rateDisplay').text(sv.rate)
+  
